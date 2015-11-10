4c6251294bd69ef04c15ddd29cd785efdf6103c3
Ivo Spasov <ivossss@yahoo.com>
2015-11-10 18:01:14 +0200

    Circuit get by id and post actions added.

--- Source/Server/LapTimes.Server.Api/Controllers/CircuitsController.cs
+++ Source/Server/LapTimes.Server.Api/Controllers/CircuitsController.cs
@@ -2,6 +2,7 @@
 {
     using System.Linq;
     using System.Web.Http;
+    using AutoMapper;
     using AutoMapper.QueryableExtensions;
     using Data.Common.Repositories;
     using Data.Models;
@@ -20,10 +21,49 @@
         {
             var circuits = this.circuitsRepository
                 .All()
-                .ProjectTo<CircuitResponseModel>()
+                .ProjectTo<CircuitRequestResponseModel>()
                 .ToList();
 
             return Ok(circuits);
+        }
+
+        public IHttpActionResult Get(int? id)
+        {
+            if (id == null)
+            {
+                return this.BadRequest("Circuit id cannot be null");
+            }
+
+            var circuitFromDb = this.circuitsRepository
+                .GetById(id);
+
+            if (circuitFromDb == null)
+            {
+                return this.NotFound();
+            }
+
+            var circuit = Mapper.Map<CircuitRequestResponseModel>(circuitFromDb);
+            return this.Ok(circuit);
+        }
+
+        // [Authorize]
+        public IHttpActionResult Post(CircuitRequestResponseModel circuit)
+        {
+            if (circuit == null)
+            {
+                return this.BadRequest("Cirucit cannot be null.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var newCircuit = Mapper.Map<Circuit>(circuit);
+            this.circuitsRepository.Add(newCircuit);
+            this.circuitsRepository.SaveChanges();
+
+            return this.Ok(newCircuit.Id);
         }
     }
 }
--- Source/Server/LapTimes.Server.Api/Models/Circuit/CircuitRequestResponseModel.cs
+++ Source/Server/LapTimes.Server.Api/Models/Circuit/CircuitRequestResponseModel.cs
@@ -1,12 +1,14 @@
 ﻿namespace LapTimes.Server.Api.Models.Circuit
 {
+    using System.ComponentModel.DataAnnotations;
     using Data.Models;
     using Infrastructure.Mapping;
 
-    public class CircuitResponseModel : IMapFrom<Circuit>
+    public class CircuitRequestResponseModel : IMapFrom<Circuit>
     {
         public int Id { get; set; }
         
+        [Required]
         public string Name { get; set; }
     }
 }
